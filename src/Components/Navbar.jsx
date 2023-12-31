98c47d7965b47c3c99220732abf619a27735f34c
jgodwin10 <easypeasy725@gmail.com>
2023-12-31 09:02:38 +0100

    car-rental

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,10 @@ import logo from "../images/logo/logo.png"
 
 const Navbar = () => {
   return (
-    <div className="">
-      <img src={logo} alt="" />
+    <div className="flex items-center justify-between mx-auto max-w-[1340px] px-5 py-6 z-30">
+      <img width={145} src={logo} alt="" />
+
+        
     </div>
   )
 }
