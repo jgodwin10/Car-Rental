75da404b5348e5432f9f9583569db8752acf02c9
jgodwin10 <easypeasy725@gmail.com>
2024-01-12 12:11:28 +0100

    Finished Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import "./App.css"
+import Home from "./Pages/Home"
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Models from './Pages/Models';
+import Testimonials from './Pages/Testimonials';
 
 
 function App() {
   return (
     <>
-     <Navbar />
-     <Outlet />
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="models" element={<Models />} />
+        <Route path="testimonials" element={<Testimonials />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
